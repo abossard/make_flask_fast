9b08a0b34f613fa5444de1de1173f5482f08709c
Andre Bossard <abossard@gmail.com>
2019-06-20 23:18:17 +0200

    japronto

--- run.sh
+++ run.sh
@@ -5,5 +5,11 @@ trap "kill 0" EXIT
 
 gunicorn -k sync -w 4 -b 127.0.0.1:4000 server:app &
 sleep 2
-boom http://127.0.0.1:4000 -n 1000 -c 100
+boom http://127.0.0.1:4000 -n 2000 -c 2000
+kill %1
+
+python server2.py &
+boom http://127.0.0.1:8080 -n 2000 -c 2000
+kill %1
+
 echo "done"
